a79fc28b3201ffdd9d60b41521102165bb21ef86
ArcaneCipher <desjardins.jarp@gmail.com>
2025-03-12 23:35:26 -0400

    Implement benchmarking function

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -3,7 +3,7 @@ def benchmark
   start_time = Time.now
   yield
   end_time = Time.now
-  running_time = end_time - start_time
+  end_time - start_time
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
